cb0cb8a5c816522d7c1528871e767b47646e83e6
upexcel <rahul.excel2011@gmail.com>
2018-10-25 12:06:53 +0530

    ios setup

---
+++ ios/Pods/Target Support Files/Pods-openstreetmap/Pods-openstreetmap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_openstreetmap : NSObject
+@end
+@implementation PodsDummy_Pods_openstreetmap
+@end
---
+++ ios/Pods/Target Support Files/Pods-openstreetmapTests/Pods-openstreetmapTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_openstreetmapTests : NSObject
+@end
+@implementation PodsDummy_Pods_openstreetmapTests
+@end
